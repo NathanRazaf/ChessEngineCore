1fdbec0f73ba14d4d0fa97c71805a3cbb7f9ba24
Asp3rity <natrazaf2020@gmail.com>
2025-12-27 09:14:08 -0500

    Remove vcpkg_installed directory from tracking

--- vcpkg_installed/vcpkg/blds/gtest/x64-windows-rel/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/pute/ChessEngineCore/vcpkg_installed/vcpkg/blds/gtest/src/v1.16.0-a2e0b328c1.clean/googletest
-# Build directory: C:/pute/ChessEngineCore/vcpkg_installed/vcpkg/blds/gtest/x64-windows-rel/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- vcpkg_installed/x64-windows/vcpkg/blds/gtest/x64-windows-rel/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/pute/ChessEngineCore/vcpkg_installed/x64-windows/vcpkg/blds/gtest/src/v1.16.0-a2e0b328c1.clean
-# Build directory: C:/pute/ChessEngineCore/vcpkg_installed/x64-windows/vcpkg/blds/gtest/x64-windows-rel
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
